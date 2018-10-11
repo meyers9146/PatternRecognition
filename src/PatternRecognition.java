28a029945b6e2ea1586baa2a82071d29adf0ea40
Mike Meyers <Mike@The-Mikebook-Pro-2.local>
2018-10-11 15:14:28 -0400

    Program debugged, final version 1.0

--- src/PatternRecognition.java
+++ src/PatternRecognition.java
@@ -5,6 +5,10 @@ public class PatternRecognition {
 		
 	Scanner input = new Scanner(System.in);
 	
+	boolean programRunning = true; //Program repeats while programRunning is true
+	
+	do {
+	
 	//Request number of rows
 	System.out.println("Enter the number of rows for the list: ");
 	int rows = input.nextInt();
@@ -15,7 +19,7 @@ public class PatternRecognition {
 	
 	//Check for a valid array size
 	while (rows < 4 || columns < 4) {
-		System.out.print("This program checks for 4 consecutive integers. Your"
+		System.out.println("This program checks for 4 consecutive integers. Your"
 				+ " array size is too small to test.");
 		//Re-request number of rows
 		System.out.println("Enter the number of rows for the list: ");
@@ -25,7 +29,8 @@ public class PatternRecognition {
 		columns = input.nextInt();
 	}
 	
-	int[][]matrix = new int[columns][rows];
+	//Create the matrix with the given columns and rows
+	int[][]matrix = new int[rows][columns];
 	
 	//Enter values for the matrix, separated by spaces. Hit Enter when finished
 	System.out.println("Please enter " + rows*columns + " integer values separated"
@@ -36,20 +41,13 @@ public class PatternRecognition {
 				matrix[i][j] = input.nextInt();
 			}
 		}
-
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				System.out.printf("%2d ", matrix[i][j]);
-			}
-			System.out.print("\n");
-		}
 	
-		input.close();
+		
 		
 	//Print the entered matrix
 	System.out.println("Here is your entered matrix:");
-	for(int i = 0; i < columns; i++) {
-		for(int j = 0; j < rows; j++) {
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++) {
 			System.out.printf("%3d ", matrix[i][j]);
 		}
 		System.out.print("\n");
@@ -58,35 +56,61 @@ public class PatternRecognition {
 	//Perform check for 4 consecutive numbers and return result
 	System.out.println("This matrix " + ((isConsecutiveFour(matrix) == true )? "has":"does not have")
 	+ " a sequence of four matching numbers.");
-		
+	
+	//Ask if user wants to run again
+	char YesNo = 'x';
+	while (!(YesNo == 'Y' || YesNo == 'y' || YesNo == 'N' || YesNo == 'n')){
+		System.out.println("Would you like to run the program again? Y/N");
+		YesNo = input.next().charAt(0);
+		}
+	if (YesNo == 'Y' || YesNo == 'y') programRunning = true;
+	else if (YesNo == 'N' || YesNo == 'n') programRunning = false;
+	
+	
+	}while(programRunning);
+	
+	input.close();
+	
+	//Print programmer name at end and end program
+	System.out.println("Programmed by Michael Meyers");
+	System.out.println("Ending program...");
+	
 	}
 	
 	public static boolean isConsecutiveFour(int[][] values) {
 	
+		//check diagonal
 		for (int i = 0; i < values.length-3; i++) {
 			for (int j = 0; j < (values[0].length-3); j++) {
-				//check diagonal
 				if (values[i][j] == values[i+1][j+1] && values[i+1][j+1] == values[i+2][j+2]
 						&& values[i+2][j+2] == values[i+3][j+3]) {
 					return true;
 				}
-				//check horizontal
-				else if (values[i][j] == values[i][j+1] && values[i][j+1] == values[i][j+2]
-						&& values[i][j+2] == values[i][j+3]) {
-					return true;
-				}
-				//check vertical
-				else if (values[i][j] == values[i+1][j] && values[i+1][j] == values[i+2][j]
-						&& values[i+2][j] == values[i+3][j]) {
-					return true;
+			}
+		}
+		//check horizontal
+		for (int i = 0; i < values.length; i++) {
+			for (int j = 0; j < values[0].length - 3; j++) {
+				if (values[i][j] == values[i][j+1] && values[i][j+1] == values[i][j+2]
+				&& values[i][j+2] == values[i][j+3]) {
+			return true;
 				}
 			}
 		}
+		//check vertical
+		for (int i = 0; i < values.length-3; i++) {
+			for (int j = 0; j < values[0].length; j++) {
+				if (values[i][j] == values[i+1][j] && values[i+1][j] == values[i+2][j]
+					&& values[i+2][j] == values[i+3][j]) {
+					return true;
+					}
+				}
+			}
 		//check opposite diagonal
-		for (int i = values.length; i > 2; i--) {
-			for (int j = values[0].length; j > 2; j--) {
-				if (values[i][j] == values [i-1][j-1] && values[i-1][j-1] == values[i-2][j-2]
-						&& values[i-2][j-2] == values[i-3][j-3]) {
+		for (int i = values.length-1; i > 2; i--) {
+			for (int j = 0; j < values[0].length-3; j++) {
+				if (values[i][j] == values [i-1][j+1] && values[i-1][j+1] == values[i-2][j+2]
+						&& values[i-2][j+2] == values[i-3][j+3]) {
 					return true;
 				}
 			}
